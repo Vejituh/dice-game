a4edaa6f79df383b381a7d427194bc9c6f21fbb9
Rafael Gomes <rafabear@bearmachine-share.broadband>
2020-10-15 08:46:39 +0100

    add doubleNothingBtn and fix display score

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const player1DiceRoll = document.getElementById("player1-dice-roll");
 const player2DiceRoll = document.getElementById("player2-dice-roll");
 const rollDiceBtn = document.getElementById("roll-dice-button");
 const resetGameBtn = document.getElementById("reset-game-button");
+const doubleNothingBtn = document.getElementById("double-nothing-button");
 
 const randomNum = () => {
     return Math.floor(Math.random() * (6 - 1 +1) +1)
@@ -19,6 +20,7 @@ const checkWinner = () => {
         rollDiceBtn.classList.add("inactive");
         resetGameBtn.classList.remove("inactive");
         resetGameBtn.classList.add("active");
+        doubleNothingBtn.classList.add("inactive");
         if (player1Score >= 20) {
             message.textContent = "Player 1 is your winner! 🎉";
         } else if (player2Score >= 20){
@@ -39,27 +41,62 @@ const resetGame = () => {
     rollDiceBtn.classList.add("active");
     resetGameBtn.classList.remove("active");
     resetGameBtn.classList.add("inactive");
+    doubleNothingBtn.classList.remove("inactive");
 }
 
-rollDiceBtn.addEventListener("click", function() {
-    if(player1Turn) {
-        player1Score += randomNum();
-        player1DiceRoll.textContent = randomNum();
+const addScore = () => {
+    if (player1Turn) {
         player1ScoreDisplay.textContent = player1Score;
         player1DiceRoll.classList.remove("current");
         player2DiceRoll.classList.add("current");
         message.textContent = "Player 2 Turn";
         player1Turn = false;
     } else {
-        player2Score += randomNum();
-        player2DiceRoll.textContent = randomNum();
         player2ScoreDisplay.textContent = player2Score;
         player2DiceRoll.classList.remove("current");
         player1DiceRoll.classList.add("current");
         message.textContent = "Player 1 Turn";
         player1Turn = true;
     }
+}
+
+rollDiceBtn.addEventListener("click", function() {
+    let currentNum = 0;
+    if(player1Turn) {
+        currentNum = randomNum()
+        player1Score += currentNum;
+        player1DiceRoll.textContent = currentNum;
+    } else {
+        currentNum = randomNum();
+        player2Score += currentNum;
+        player2DiceRoll.textContent = currentNum;
+    }
+    addScore();
     checkWinner();
 })
 
 resetGameBtn.addEventListener("click", resetGame);
+
+doubleNothingBtn.addEventListener("click", function() {
+    const doubleNum = Math.floor(Math.random() * (7 - 1 +1));
+    if (doubleNum > 0) {
+        if(player1Turn) {
+            player1Score *= doubleNum;
+            player1DiceRoll.textContent = doubleNum;
+        } else {
+            player2Score *= doubleNum;
+            player2DiceRoll.textContent = doubleNum;
+        }
+        addScore();
+        checkWinner();
+    } else {
+        if (player1Turn) {
+            player1Score = 0;
+            player1DiceRoll.textContent = doubleNum;
+        } else {
+            player2Score = 0;
+            player2DiceRoll.textContent = doubleNum;
+        }
+        addScore();
+    }
+})
